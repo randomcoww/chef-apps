256607df65e7f4949eee620fbd36e78fcf3f19c2
randomcoww <etherblimp@gmail.com>
2017-03-10 19:18:30 -0800

    test dns vip

--- environment/attributes/default.rb
+++ environment/attributes/default.rb
@@ -1,5 +1,5 @@
 node.default['environment']['lan_subnet'] = "192.168.62.0/23"
-node.default['environment']['lan_vip_gateway'] = "192.168.62.240"
+node.default['environment']['lan_vip_gateway'] = "192.168.62.240/23"
 node.default['environment']['lan_subnet_dhcp'] = "192.168.62.32/27"
 node.default['environment']['lan_if'] = "brlan"
 node.default['environment']['lan_vrrp_state'] = "MASTER"
