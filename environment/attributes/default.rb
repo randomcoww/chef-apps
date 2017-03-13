dc4225ce7b8a10165ee95074330a16500e9ab61c
randomcoww <etherblimp@gmail.com>
2017-03-12 17:09:03 -0700

    add gateway and dns ip

--- environment/attributes/default.rb
+++ environment/attributes/default.rb
@@ -1,5 +1,6 @@
 node.default['environment']['lan_subnet'] = "192.168.62.0/23"
 node.default['environment']['lan_vip_gateway'] = "192.168.62.240/23"
+node.default['environment']['lan_ip_gateway'] = "192.168.62.240"
 node.default['environment']['lan_subnet_dhcp'] = "192.168.62.32/27"
 node.default['environment']['lan_if'] = "brlan"
 node.default['environment']['lan_vrrp_state'] = "MASTER"
@@ -14,3 +15,4 @@ node.default['environment']['wan_if'] = "eth1"
 
 node.default['environment']['dns_if'] = "eth0"
 node.default['environment']['dns_vip'] = "192.168.62.250/23"
+node.default['environment']['dns_ip'] = "192.168.62.250"
