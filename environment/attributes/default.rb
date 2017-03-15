f40370ef89c2c003bfcbee5b666534b8936fc316
randomcoww <etherblimp@gmail.com>
2017-03-14 23:20:28 -0700

    libvirt mac address

--- environment/attributes/default.rb
+++ environment/attributes/default.rb
@@ -12,6 +12,7 @@ node.default['environment']['vpn_subnet'] = "192.168.30.0/23"
 node.default['environment']['vpn_subnet_dhcp'] = "192.168.30.32/27"
 
 node.default['environment']['wan_if'] = "eth1"
+node.default['environment']['wan_mac'] = "52:54:00:63:6e:b0"
 
 node.default['environment']['dns_if'] = "eth0"
 node.default['environment']['dns_ip'] = "192.168.62.250"
