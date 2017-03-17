d735dc0901ce2a93654d68f77261fba42931fad3
randomcoww <etherblimp@gmail.com>
2017-03-16 21:59:35 -0700

    updates

--- environment/attributes/default.rb
+++ environment/attributes/default.rb
@@ -1,5 +1,5 @@
 node.default['environment']['lan_if'] = "brlan"
-node.default['environment']['lan_ip_gateway'] = "192.168.62.240"
+node.default['environment']['lan_vip_gateway'] = "192.168.62.240"
 node.default['environment']['lan_subnet'] = "192.168.62.0/23"
 node.default['environment']['lan_subnet_dhcp'] = "192.168.62.32/27"
 node.default['environment']['lan_vrrp_state'] = "MASTER"
@@ -14,8 +14,8 @@ node.default['environment']['wan_if'] = "eth1"
 node.default['environment']['wan_mac'] = "52:54:00:63:6e:b0"
 
 node.default['environment']['dns_if'] = "eth0"
-node.default['environment']['dns_ip'] = "192.168.62.250"
-node.default['environment']['dns_vip'] = "192.168.62.250/23"
+node.default['environment']['dns_vip'] = "192.168.62.250"
+node.default['environment']['dns_ip'] = "192.168.62.252/23"
 node.default['environment']['dns_ha_state'] = "MASTER"
 node.default['environment']['dns_ha_id'] = 21
 node.default['environment']['dns_ha_priority'] = 200
