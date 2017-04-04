3a17f9eb1679f7d7ee205d3d92c2be17a2f1af86
randomcoww <etherblimp@gmail.com>
2017-04-04 00:37:37 -0700

    add environment resource

---
+++ environment_resource/metadata.rb
@@ -0,0 +1,8 @@
+name             'environment_resource'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures environment_resource'
+long_description 'Installs/Configures environment_resource'
+version          '0.1.0'
+depends          'dbag'
---
+++ environment_resource/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: environment_resource
+# Recipe:: default
+#
+# Copyright (C) 2017 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
