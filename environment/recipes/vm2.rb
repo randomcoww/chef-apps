dadac70e063427ac877735daadcd37acb05c1469
randomcoww <etherblimp@gmail.com>
2017-04-04 22:45:41 -0700

    cleanup

--- environment/recipes/vm2.rb
+++ environment/recipes/vm2.rb
@@ -1,4 +1,4 @@
-node.default['environment_v2']['host_lan_if'] = "eno1"
-node.default['environment_v2']['host_vpn_if'] = "vpn"
-node.default['environment_v2']['host_wan_if'] = "wan"
-node.default['environment_v2']['host_store_if'] = "enp7s0"
+node.override['environment_v2']['host_lan_if'] = "eno1"
+node.override['environment_v2']['host_vpn_if'] = "vpn"
+node.override['environment_v2']['host_wan_if'] = "wan"
+node.override['environment_v2']['host_store_if'] = "enp7s0"
