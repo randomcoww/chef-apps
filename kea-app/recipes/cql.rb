a4b9f7783b39e7b58c34ed70a5e75f61824391cc
randomcoww <etherblimp@gmail.com>
2017-04-15 01:00:12 -0700

    kea docker reload on config update

--- kea-app/recipes/cql.rb
+++ kea-app/recipes/cql.rb
@@ -2,7 +2,7 @@ kea_config 'kea-cql' do
   config node['kea']['cql']['kea_config']
   path ::File.join(Chef::Config[:file_cache_path], 'kea-cql')
   action :create
-  notifies :restart, "docker_container[kea-cql]", :delayed
+  notifies :reload, "docker_container[kea-cql]", :delayed
 end
 
 docker_image 'randomcoww/kea-cql' do
