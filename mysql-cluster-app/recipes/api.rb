7da6960673c2dac27f30f93c92f5c72ae226b393
randomcoww <etherblimp@gmail.com>
2017-04-30 15:58:34 -0700

    use debconf to feed initial password

--- mysql-cluster-app/recipes/api.rb
+++ mysql-cluster-app/recipes/api.rb
@@ -3,16 +3,28 @@ execute "pkg_update" do
   action :run
 end
 
+bash "debconf_root-pass" do
+  code %Q{debconf-set-selections <<< "mysql-cluster-community-server mysql-cluster-community-server/root-pass password #{node['mysql-cluster']['api']['root_password']}"}
+  action :nothing
+end
+
+bash "debconf_re-root-pass" do
+  code %Q{debconf-set-selections <<< "mysql-cluster-community-server mysql-cluster-community-server/re-root-pass password #{node['mysql-cluster']['api']['root_password']}"}
+  action :nothing
+end
+
 ## service starts automatically with default configs on install
 ## this conflicts with unbound running on default port
 ## stop until configs are written to run on another port
 apt_package node['mysql-cluster']['api']['pkg_names'] do
   action :install
-  notifies :stop, "service[mysql]", :immediately
   options [
     '--no-install-recommends',
     '--allow-unauthenticated'
   ]
+  notifies :run, "bash[debconf_root-pass]", :before
+  notifies :run, "bash[debconf_re-root-pass]", :before
+  notifies :stop, "service[mysql]", :immediately
 end
 
 mysql_cluster_config 'api' do
