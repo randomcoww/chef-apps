d5fa517046b0551cba7499a6afc18530b2448116
randomcoww <etherblimp@gmail.com>
2017-05-02 03:39:33 -0700

    remaining dns entries

--- environment/attributes/v2.rb
+++ environment/attributes/v2.rb
@@ -91,6 +91,20 @@ node.default['environment_v2']['host']['haproxy2'] = {
   'ip_lan' => "192.168.62.222"
 }
 
+node.default['environment_v2']['host']['vm1-ipmi'] = {
+  'ip_lan' => '192.168.63.64'
+}
+
+node.default['environment_v2']['host']['vm2-ipmi'] = {
+  'ip_lan' => '192.168.63.63'
+}
+
+node.default['environment_v2']['host']['sw'] = {
+  'ip_lan' => '192.168.63.95'
+}
+
+
+
 
 ## load current host under 'current_host'
 node.default['environment_v2']['current_host'] = node['environment_v2']['host'][node['hostname']]
