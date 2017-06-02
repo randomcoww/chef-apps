6ba6414f89d96481d349aefad8b92d5d5465db0f
randomcoww <etherblimp@gmail.com>
2017-06-02 02:17:11 -0700

    remove old kube recipes

--- kube_master/recipes/kubectl.rb
+++
@@ -1,5 +0,0 @@
-remote_file node['kube_master']['kubectl']['binary_path'] do
-  source node['kube_master']['kubectl']['remote_file']
-  mode '0750'
-  action :create_if_missing
-end
--- kube_master/recipes/manifests.rb
+++
@@ -1,6 +0,0 @@
-node['kube_master']['manifests'].each do |f, config|
-  kubernetes_pod ::File.join(node['kube_master']['manifests_path'], f) do
-    config config
-    action :create
-  end
-end
