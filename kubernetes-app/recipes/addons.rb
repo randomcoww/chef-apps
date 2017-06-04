92cf00f8f15a3b64cee6d7d7b9efafe4ce530517
randomcoww <etherblimp@gmail.com>
2017-06-03 22:23:55 -0700

    manifest changes, chef interval at 5min

--- kubernetes-app/recipes/addons.rb
+++ kubernetes-app/recipes/addons.rb
@@ -2,17 +2,20 @@ node['kubernetes']['addons'].each do |f, config|
   kubernetes_pod ::File.join(node['kubernetes']['addons_path'], f) do
     config config
     action :create
+    subscribes :restart, "kubernetes_ca[ca]", :immediately
   end
 end
 
-Dir.entries(node['kubernetes']['addons_path']).each do |f|
-  next if node['kubernetes']['addons'].has_key?(f)
+if ::File.directory?(node['kubernetes']['addons_path'])
 
-  path = ::File.join(node['kubernetes']['addons_path'], f)
-  next unless ::File.file?(path)
+  Dir.entries(node['kubernetes']['addons_path']).each do |f|
+    next if node['kubernetes']['addons'].has_key?(f)
 
-  kubernetes_pod path do
-    config ({})
-    action :delete
+    path = ::File.join(node['kubernetes']['addons_path'], f)
+    next unless ::File.file?(path)
+
+    kubernetes_pod path do
+      action :delete
+    end
   end
 end
--- kubernetes-app/recipes/static_pods.rb
+++ kubernetes-app/recipes/static_pods.rb
@@ -2,17 +2,19 @@ node['kubernetes']['static_pods'].each do |f, config|
   kubernetes_pod ::File.join(node['kubernetes']['manifests_path'], f) do
     config config
     action :create
+    subscribes :restart, "kubernetes_ca[ca]", :immediately
   end
 end
 
-Dir.entries(node['kubernetes']['manifests_path']).each do |f|
-  next if node['kubernetes']['static_pods'].has_key?(f)
+if ::File.directory?(node['kubernetes']['manifests_path'])
+  Dir.entries(node['kubernetes']['manifests_path']).each do |f|
+    next if node['kubernetes']['static_pods'].has_key?(f)
 
-  path = ::File.join(node['kubernetes']['manifests_path'], f)
-  next unless ::File.file?(path)
+    path = ::File.join(node['kubernetes']['manifests_path'], f)
+    next unless ::File.file?(path)
 
-  kubernetes_pod path do
-    config ({})
-    action :delete
+    kubernetes_pod path do
+      action :delete
+    end
   end
 end
