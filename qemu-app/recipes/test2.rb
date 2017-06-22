f991e85be9c760e5c424fbcbd310d6bf80ec7e7e
randomcoww <etherblimp@gmail.com>
2017-06-22 03:09:35 -0700

    kubelet deploy

---
+++ qemu-app/recipes/test2.rb
@@ -0,0 +1,2 @@
+node.default['qemu']['current_config']['hostname'] = 'test2'
+include_recipe "qemu-app::template_test"
---
+++ qemu-app/recipes/test3.rb
@@ -0,0 +1,2 @@
+node.default['qemu']['current_config']['hostname'] = 'test3'
+include_recipe "qemu-app::template_test"
