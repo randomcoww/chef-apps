90776665e21a08a2ad6b73451ed5ba73a3b2aa72
randomcoww <etherblimp@gmail.com>
2017-06-24 03:25:15 -0700

    fix kubelet docker

--- kubelet-app/attributes/default.rb
+++ kubelet-app/attributes/default.rb
@@ -1,8 +1,7 @@
-node.override['kubernetes']['docker']['systemd_dropin'] = {
+node.default['kubernetes']['docker']['systemd_dropin'] = {
   'Service' => {
     "Restart" => 'always',
     "RestartSec" => 5,
-    "EnvironmentFile" => node['kubernetes']['flannel']['environment']['FLANNELD_SUBNET_FILE'],
     "ExecStart" => [
       '',
       "/usr/bin/dockerd -H fd:// --log-driver=journald --ip-masq=false --iptables=false"
