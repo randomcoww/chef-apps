573240f0d1b047dfadd52a4a7f2a3486d64864b8
randomcoww <etherblimp@gmail.com>
2017-08-04 19:51:34 -0700

    add node ports to haproxy

--- container/haproxy-pod/recipes/_haproxy.rb
+++ container/haproxy-pod/recipes/_haproxy.rb
@@ -28,14 +28,44 @@ node.default['kubelet']['haproxy']['config'] = HaproxyHelper::ConfigGenerator.ge
     ],
     'stats' => 'uri /haproxy-status'
   },
-  # 'frontend mysql' => {
-  #   'default_backend' => 'mysql',
-  #   'bind' => "*:3306",
-  #   'maxconn' => 2000
-  # },
-  # 'backend mysql' => {
-  #   'server' => node['environment_v2']['set']['kea-mysql']['hosts'].map { |e|
-  #       "#{e} #{node['environment_v2']['host'][e]['ip_lan']}:3306 check"
-  #     }
-  # }
+  'frontend transmission' => {
+    'default_backend' => 'transmission',
+    'bind' => "*:9091",
+    'maxconn' => 2000
+  },
+  'backend transmission' => {
+    'server' => node['environment_v2']['set']['haproxy']['hosts'].map { |e|
+        "#{e} #{node['environment_v2']['host'][e]['ip_lan']}:30063 check"
+      }
+  },
+  'frontend sshd' => {
+    'default_backend' => 'sshd',
+    'bind' => "*:2222",
+    'maxconn' => 2000
+  },
+  'backend sshd' => {
+    'server' => node['environment_v2']['set']['haproxy']['hosts'].map { |e|
+        "#{e} #{node['environment_v2']['host'][e]['ip_lan']}:32222 check"
+      }
+  },
+  'frontend mpd_control' => {
+    'default_backend' => 'mpd_control',
+    'bind' => "*:6600",
+    'maxconn' => 2000
+  },
+  'backend mpd_control' => {
+    'server' => node['environment_v2']['set']['haproxy']['hosts'].map { |e|
+        "#{e} #{node['environment_v2']['host'][e]['ip_lan']}:30061 check"
+      }
+  },
+  'frontend mpd_stream' => {
+    'default_backend' => 'mpd_stream',
+    'bind' => "*:8000",
+    'maxconn' => 2000
+  },
+  'backend mpd_stream' => {
+    'server' => node['environment_v2']['set']['haproxy']['hosts'].map { |e|
+        "#{e} #{node['environment_v2']['host'][e]['ip_lan']}:30062 check"
+      }
+  }
 })
