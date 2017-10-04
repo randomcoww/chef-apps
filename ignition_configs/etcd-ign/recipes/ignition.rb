335974d24ea02b19b597248b2f57202e16f1af81
randomcoww <randomcoww@desktop.hl.lan>
2017-10-04 02:57:21 -0700

    etcd dns bootstrap

--- ignition_configs/etcd-ign/recipes/ignition.rb
+++ ignition_configs/etcd-ign/recipes/ignition.rb
@@ -10,22 +10,23 @@ base = {
   }
 }
 
+domain = [
+  node['environment_v2']['domain']['host_lan'],
+  node['environment_v2']['domain']['top']
+].join('.')
+
 
 node['ignition']['etcd']['hosts'].each do |host|
 
   ip_lan = node['environment_v2']['host'][host]['ip_lan']
   if_lan = node['environment_v2']['host'][host]['if_lan']
+  host_record = [host, domain].join('.')
 
   files = [
     {
       "path" => "/etc/hostname",
       "mode" => 420,
       "contents" => "data:,#{host}"
-    },
-    {
-      "path" => "/opt/bin/setup-network-environment",
-      "mode" => 493,
-      "contents" => node['environment_v2']['url']['setup_network_environment']
     }
   ]
 
@@ -51,10 +52,6 @@ node['ignition']['etcd']['hosts'].each do |host|
     }
   ]
 
-  etcd_initial_cluster = node['environment_v2']['set']['etcd']['hosts'].map { |h|
-    "#{h}=http://#{node['environment_v2']['host'][h]['ip_lan']}:2380"
-  }
-
   systemd = [
     {
       "name" => "etcd-member",
@@ -62,42 +59,22 @@ node['ignition']['etcd']['hosts'].each do |host|
         {
           "name" => "etcd-env",
           "contents" => {
-            "Unit" => {
-              "Requires" => "setup-network-environment.service",
-              "After" => "setup-network-environment.service"
-            },
             "Service" => {
               "Environment" => [
                 %Q{ETCD_OPTS="#{[
-                  "--name=#{host}",
-                  "--initial-advertise-peer-urls=http://${DEFAULT_IPV4}:2380",
-                  "--listen-peer-urls=http://${DEFAULT_IPV4}:2380",
-                  "--listen-client-urls=http://${DEFAULT_IPV4}:2379,http://127.0.0.1:2379",
-                  "--advertise-client-urls=http://${DEFAULT_IPV4}:2379",
-                  "--discovery=#{node['environment_v2']['url']['etcd_discovery']}",
-                  # "--initial-cluster=#{etcd_initial_cluster.join(',')}",
-                  # "--initial-cluster-state=existing",
-                  # "--initial-cluster-token=etcd-1"
+                  "--discovery-srv=#{domain}",
+                  "--initial-advertise-peer-urls=http://#{host_record}:2380",
+                  "--listen-peer-urls=http://#{host_record}:2380",
+                  "--listen-client-urls=http://#{host_record}:2379,http://127.0.0.1:2379",
+                  "--advertise-client-urls=http://#{host_record}:2379",
+                  "--initial-cluster-state=new",
+                  "--initial-cluster-token=etcd-1"
                 ].join(' ')}"}
               ]
             }
           }
         }
       ]
-    },
-    {
-      "name" => "setup-network-environment",
-      "contents" => {
-        "Unit" => {
-          "Requires" => "network-online.target",
-          "After" => "network-online.target"
-        },
-        "Service" => {
-          "Type" => "oneshot",
-          "ExecStart" => "/opt/bin/setup-network-environment",
-          "RemainAfterExit" => "yes"
-        }
-      }
     }
   ]
 
