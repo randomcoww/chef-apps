98f868f1119e4e894be962f79bc2f04cb4c52784
randomcoww <etherblimp@gmail.com>
2018-03-14 03:25:51 -0700

    add cfssl deploy cookbook

---
+++ kube_manifests/cfssl-manifest/README.md
@@ -0,0 +1,4 @@
+# cfssl-manifest
+
+TODO: Enter the cookbook description here.
+
---
+++ kube_manifests/cfssl-manifest/recipes/manifest.rb
@@ -0,0 +1,53 @@
+cfssl_manifest = {
+  "apiVersion" => "v1",
+  "kind" => "Pod",
+  "metadata" => {
+    "name" => "cfssl"
+  },
+  "spec" => {
+    "restartPolicy" => "Always",
+    # "hostNetwork" => true,
+    "containers" => [
+      {
+        "name" => "cfssl",
+        "image" => node['kube']['images']['cfssl'],
+        "args" => [
+          "serve",
+          "-address",
+          "0.0.0.0",
+          "-ca",
+          "/certs/root_ca/root_ca.pem",
+          "-ca-key",
+          "/certs/root_ca/root_ca-key.pem",
+          "-config",
+          "/certs/config.json",
+        ],
+        "volumeMounts" => [
+          {
+            "name" => "certs",
+            "mountPath" => "/certs"
+          }
+        ],
+        "ports" => [
+          {
+            "containerPort" => 8888,
+            "hostPort" => 8888,
+            "protocol" => "TCP"
+          }
+        ]
+      }
+    ],
+    "volumes" => [
+      {
+        "name" => "certs",
+        "hostPath" => {
+          "path" => "/data/certs"
+        }
+      }
+    ]
+  }
+}
+
+node['environment_v2']['set']['ca']['hosts'].each do |host|
+  node.default['kubernetes']['static_pods'][host]['cfssl'] = cfssl_manifest
+end
