37be0c1a66c4dda1d3dfe8dfedba455bb656c7ac
randomcoww <etherblimp@gmail.com>
2018-03-14 23:17:57 -0700

    remove openssl cookbook

--- kube_manifests/kube_master-manifest/metadata.rb
+++ kube_manifests/kube_master-manifest/metadata.rb
@@ -20,5 +20,3 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # source_url 'https://github.com/<insert_org_here>/kube_master-manifest'
 depends          'environment'
 depends          'haproxy_resource'
-depends          'openssl_cert'
-depends          'dbag'
